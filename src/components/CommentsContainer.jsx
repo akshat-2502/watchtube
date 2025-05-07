8a4f5301e777c1df75145d5997fb47febb7ceb92
akshat-2502 <khulbeakshat@gmail.com>
2025-05-08 02:17:40 +0530

    comment and comment replies done but hard code data

--- src/components/CommentsContainer.jsx
+++ src/components/CommentsContainer.jsx
@@ -1,9 +1,136 @@
 import React from "react";
+import { FaRegCircleUser } from "react-icons/fa6";
+
+const commentsData = [
+  {
+    name: "Akshat Khulbe",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+    replies: [
+      {
+        name: "Akshat Khulbe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+        replies: [],
+      },
+      {
+        name: "Akshat Khulbe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+        replies: [
+          {
+            name: "Akshat Khulbe",
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+            replies: [
+              {
+                name: "Akshat Khulbe",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+                replies: [],
+              },
+            ],
+          },
+          {
+            name: "Akshat Khulbe",
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+            replies: [
+              {
+                name: "Akshat Khulbe",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+                replies: [],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Akshat Khulbe",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+    replies: [
+      {
+        name: "Akshat Khulbe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+        replies: [
+          {
+            name: "Akshat Khulbe",
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Akshat Khulbe",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+    replies: [
+      {
+        name: "Akshat Khulbe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+        replies: [],
+      },
+      {
+        name: "Akshat Khulbe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+        replies: [
+          {
+            name: "Akshat Khulbe",
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+            replies: [],
+          },
+        ],
+      },
+      {
+        name: "Akshat Khulbe",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "Akshat Khulbe",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+    replies: [],
+  },
+  {
+    name: "Akshat Khulbe",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+    replies: [],
+  },
+  {
+    name: "Akshat Khulbe",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptas error cupiditate, aliquid laborum excepturi.",
+    replies: [],
+  },
+];
+
+const Comment = ({ data }) => {
+  const { name, text, replies } = data;
+  return (
+    <div className="flex bg-gray-200 items-center p-2 my-2">
+      <FaRegCircleUser className="w-8 h-8" />
+      <div className="p-3">
+        <p className="font-bold">{name}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentList = ({ comments }) => {
+  return comments.map((comment, index) => (
+    <div>
+      <Comment key={index} data={comment} />
+      <div className="pl-5 ml-5 border-l-1 border-l-black">
+        <CommentList comments={comment.replies} />
+      </div>
+    </div>
+  ));
+};
 
 const CommentsContainer = () => {
   return (
-    <div className="mt-5">
+    <div className="mt-5 w-[1000px]">
       <h1 className="text-xl font-bold">Comments :- </h1>
+      <CommentList comments={commentsData} />
     </div>
   );
 };
