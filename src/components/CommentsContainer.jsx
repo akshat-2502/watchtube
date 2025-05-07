dbe61fb513d7931ef963d032487ab42ae2d0e679
akshat-2502 <khulbeakshat@gmail.com>
2025-05-08 00:52:37 +0530

    comment work started

---
+++ src/components/CommentsContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CommentsContainer = () => {
+  return (
+    <div className="mt-5">
+      <h1 className="text-xl font-bold">Comments :- </h1>
+    </div>
+  );
+};
+
+export default CommentsContainer;
--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -12,17 +13,20 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
   return (
-    <div className="p-5">
-      <iframe
-        width="1000"
-        height="500"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
+    <div>
+      <div className="p-5">
+        <iframe
+          width="1000"
+          height="500"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+        <CommentsContainer />
+      </div>
     </div>
   );
 };
