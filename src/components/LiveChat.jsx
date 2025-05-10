f32ecaaf8b1e66237f3034ffd3a186853a2c13f6
akshat-2502 <khulbeakshat@gmail.com>
2025-05-10 21:32:20 +0530

    live chat send option skeleton

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -3,6 +3,7 @@ import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomName, randomString } from "../utils/helper";
+import { FiSend } from "react-icons/fi";
 
 const LiveChat = () => {
   const dispatch = useDispatch();
@@ -23,13 +24,23 @@ const LiveChat = () => {
   }, []);
   return (
     <>
-      <div className="border-1 bg-gray-100 border-gray-300 w-full h-[500px] overflow-y-scroll flex flex-col-reverse">
-        {chatMessages.map((c, index) => (
-          <ChatMessage key={index} name={c.name} message={c.message} />
-        ))}
-      </div>
-      <div>
-        <input className="border-1 border-zinc-500" type="text" />
+      <div className="border-1 bg-gray-100 border-gray-300">
+        <div className=" w-full h-[456px] overflow-y-scroll flex flex-col-reverse">
+          {chatMessages.map((c, index) => (
+            <ChatMessage key={index} name={c.name} message={c.message} />
+          ))}
+        </div>
+        <hr className="text-zinc-400" />
+        <div className="flex items-center">
+          <input
+            placeholder="Type Here ..."
+            className=" p-2 outline-none border-zinc-500 w-[85%]"
+            type="text"
+          />
+          <button className="hover:cursor-pointer">
+            <FiSend className="w-6 h-6 text-zinc-400 hover:text-zinc-600" />
+          </button>
+        </div>
       </div>
     </>
   );
